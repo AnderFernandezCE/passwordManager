0495792d283ef17c45f31a8ccb43e3ee865e13ee
Ander Fernandez <anderfernandez.ce@gmail.com>
2023-12-06 15:17:29 +0100

    first commit

---
+++ tests/encryption.py
@@ -0,0 +1,17 @@
+def hash():
+    pass
+
+def key_derivation_function():
+    pass
+
+def hmac_based_extract_expand_key_derivation_function_hkdf():
+    pass
+
+def symmetric_key_generation():
+    pass
+
+def initial_vector_generation():
+    pass
+
+def aes_encryption():
+    pass
