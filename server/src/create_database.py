9b667df9a6e4584073a0b7997fca73c333e36d3d
Ander Fernandez <anderfernandez.ce@gmail.com>
2023-12-27 17:11:00 +0100

    init db model + table creation

---
+++ server/src/create_database.py
@@ -0,0 +1,26 @@
+import pymysql
+pymysql.install_as_MySQLdb()
+
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine
+from sqlalchemy.exc import OperationalError
+
+from src.config import settings
+from src.models import get_Base
+
+
+def main():
+    try:
+        dsn = str(settings.DATABASE_URL)
+        engine = create_engine(dsn, echo = True)
+        Base = get_Base()
+        Base.metadata.create_all(engine)
+    except OperationalError as e:
+        print('Base de datos fuera de servicio')
+        raise
+    except Exception as e:
+        print(e)
+    
+
+if __name__ == "__main__":
+    main()
---
+++ server/src/models.py
@@ -0,0 +1,51 @@
+from sqlalchemy import  Column, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import (
+    Integer, String, Date, DateTime, Float, Boolean, Text, BOOLEAN, Unicode, UUID, LargeBinary, CHAR)
+
+Base = declarative_base()
+
+class Users(Base):
+    __tablename__ = "users"
+
+    UUID = Column(CHAR(36), primary_key=True)
+    username = Column('username', String(50))
+    email = Column('email', String(250))
+    userhash = Column('userhash', Unicode(64))
+    key = Column('key', Unicode(128))
+    publicKey = Column('publicKey', String(4000))
+    privateKey = Column('privateKey', String(4000))
+    created_at = Column('created_at', DateTime)
+    salt = Column('salt', LargeBinary(32))
+    verified =Column('verified', Boolean)
+    
+
+# class Link(Base):
+#     __tablename__ = "link"
+
+#     id = Column(Integer, primary_key=True)
+#     url = Column('url', Text())
+#     project_id = Column(Integer, ForeignKey('project.id')) 
+
+# class Repo(Base):
+#     __tablename__ = "repo"
+
+#     id = Column(Integer, primary_key=True)
+#     domain = Column('domain', String(200), unique=True)
+#     description = Column('description', String(200))
+#     lastupdate = Column('lastupdate', DateTime)
+#     librarysoft = Column('librarysoft', String(100))
+
+# class Availability(Base):
+#     __tablename__ = "availability"
+
+#     project_id = Column(Integer, ForeignKey('project.id'), primary_key=True) 
+#     downloaded = Column('downloaded', BOOLEAN)
+#     availability_status = Column('availability_status', BOOLEAN)
+#     description = Column('description', String(200))
+#     last_check = Column('last_check', DateTime)
+#     embeddings_generated = Column('embeddings_generated', BOOLEAN)
+#     embedded = Column('embedded', BOOLEAN)
+
+def get_Base():
+  return Base
